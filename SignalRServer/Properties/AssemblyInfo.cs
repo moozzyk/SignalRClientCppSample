3f7b216ec8c916b9ad4998da9477795917d36f1a
Pawel Kadluczka <moozzyk@gmail.com>
2015-02-08 22:31:54 -0800

    Creating chat hub web app

--- SignalRServer/Properties/AssemblyInfo.cs
+++ SignalRServer/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("72546c09-f4f8-404f-a632-cd77345e709f")]
+[assembly: Guid("64b40dbb-f6c1-45b4-8fc4-764526e9f16c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
